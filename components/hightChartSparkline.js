ec369f321cc1204ea2dfb976f76b5856db830a1c
Aamir Shahzad <53485712+aamir-shahzad1@users.noreply.github.com>
2019-12-22 02:05:35 +0500

    highChartSparkJs component code added

---
+++ components/hightChartSparkline.js
@@ -0,0 +1,176 @@
+import React, { Component } from "react";
+import * as JQuery from "jquery";
+const $ = JQuery.default;
+var Highcharts = require("highcharts");
+require("highcharts/modules/exporting")(Highcharts);
+
+const defaultOptions = {
+  chart: {
+    backgroundColor: null,
+    borderWidth: 0,
+    type: 'area',
+    margin: [2, 0, 2, 0],
+    // width: 220,
+    // height: 20,
+    style: {
+      overflow: 'visible'
+    },
+
+    // small optimalization, saves 1-2 ms each sparkline
+    skipClone: true
+  },
+  title: {
+    text: ''
+  },
+  credits: {
+    enabled: false
+  },
+  xAxis: {
+    labels: {
+      enabled: false
+    },
+    title: {
+      text: null
+    },
+    startOnTick: false,
+    endOnTick: false,
+    tickPositions: []
+  },
+  yAxis: {
+    endOnTick: false,
+    startOnTick: false,
+    labels: {
+      enabled: false
+    },
+    title: {
+      text: null
+    },
+    tickPositions: [0]
+  },
+  legend: {
+    enabled: false
+  },
+  tooltip: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    hideDelay: 0,
+    shared: true,
+    padding: 8,
+    borderColor: 'silver',
+    borderRadius: 3,
+    positioner: function (w, h, point) {
+      return { x: point.plotX - w / 2, y: point.plotY - h };
+    }
+  },
+  plotOptions: {
+    series: {
+      animation: false,
+      lineWidth: 1,
+      shadow: false,
+      states: {
+        hover: {
+          lineWidth: 1
+        }
+      },
+      marker: {
+        radius: 1,
+        states: {
+          hover: {
+            radius: 2
+          }
+        }
+      },
+      fillOpacity: 0.25
+    },
+    column: {
+      negativeColor: '#910000',
+      borderColor: 'silver'
+    }
+  },
+
+  series: [{
+    data: [1,2,3]
+  }]
+};
+class HighChartComponent extends Component {
+  componentDidMount() {
+    var title = {
+      text: "Monthly Average Temperature"
+    };
+    var subtitle = {
+      text: "Source: WorldClimate.com"
+    };
+    var xAxis = {
+      categories: [
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+        "Sun"
+      ]
+    };
+    var yAxis = {
+      title: {
+        text: "Temperature (\xB0C)"
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+          color: "#808080"
+        }
+      ]
+    };
+    var tooltip = {
+      // valueSuffix: ""
+    };
+    var legend = {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
+      borderWidth: 0
+    };
+    var series = [
+      {
+        name: "Orders",
+        data: [
+          7.0,
+          6.9,
+          9.5,
+          14.5,
+          18.2,
+          21.5,
+          25.2,
+        ]
+      },
+      
+      
+    ];
+    const json = {};
+    json.title = title;
+    json.legend = legend;
+    json.subTitle = subtitle;
+    json.series = series;
+    json.tooltip = tooltip;
+    json.xAxis = xAxis;
+    json.yAxis = yAxis;
+    this.setState({ json });
+    // const rootElement = document.getElementById("container");
+    // Highcharts.chart("container", this.state.json);
+    // $("#container").chart(this.state.json);
+    const options = Highcharts.merge(defaultOptions, json)
+    Highcharts.chart("container", options);
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <h1>Hello there</h1>
+      </div>
+    );
+  }
+}
+
+export default HighChartComponent;
